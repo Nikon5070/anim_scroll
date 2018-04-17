38377cc6921d30ae84ecdfa6e0c2e7c88f0eb626
nikon5070 <nikon5070@gmail.com>
2018-04-18 07:23:45 +1000

    img, sass, html loader webpack

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+import someText from './welcome';
+import $ from 'jquery';
+import _ from 'lodash';
+
+console.log(someText('sdfgsdgsdg'));
+
+
+
+$(function() {
+    var a = 0;
+
+    var text = $('h3');
+    console.log('dd');
+    text.mouseenter(function() {
+
+        console.log((a++));
+        text.text(makeid);
+        text.animate({
+            opacity: 0.25,
+            bottom: "+=50",
+            // width: "toggle"
+        });
+
+    });
+
+
+
+});
+
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
